07b573a7a2a276052a7b05982d01c8cb464c92c4
Daniel Baccarin <danielbaccarin@outlook.com>
2025-06-17 18:29:44 -0300

    Implementing Red Test

---
+++ RoomBookingApp/RoomBookingApp.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.Tests
+{
+    public class RoomBookingRequestProcessorTest
+    {
+
+        [Fact]
+        public void Should_Return_Room_Booking_Response_With_Request_Values()
+        {
+            //Arrange
+            var request = new RoomBookingRequest()
+            {
+                FullName = "Name Test",
+                Email = "test@email.com",
+                Date = new DateTime(2025, 6, 17)
+            };
+
+            var processor = new RoomBookingRequestProcessor();
+
+            //Act
+            RoomBookingResult result = processor.BookRoom(request);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(request.FullName, result.FullName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+    }
+}
