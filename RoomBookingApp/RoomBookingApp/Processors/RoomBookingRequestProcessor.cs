9240b468fc90e684aac520495389a093f7b2c685
Daniel Baccarin <danielbaccarin@outlook.com>
2025-06-18 17:29:09 -0300

    Implementing the return of room id on book room processor (green test)

--- RoomBookingApp/RoomBookingApp/Processors/RoomBookingRequestProcessor.cs
+++ RoomBookingApp/RoomBookingApp/Processors/RoomBookingRequestProcessor.cs
@@ -27,7 +27,14 @@ namespace RoomBookingApp.Processors
             var availableRooms = _roomBookingService.GetAvailableRooms(request.Date);
 
             if (availableRooms.Any())
-                _roomBookingService.Save(CreateRoomBookingObject<RoomBooking>(request));
+            {
+                var room = availableRooms.First();
+                var roomBooking = CreateRoomBookingObject<RoomBooking>(request);
+                roomBooking.RoomId = room.Id;
+
+                _roomBookingService.Save(roomBooking);
+
+            }
 
             return CreateRoomBookingObject<RoomBookingResult>(request);
         }
